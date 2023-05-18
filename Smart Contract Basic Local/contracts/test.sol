6b9997be8a7b27c6ead6add4c32cafeb1956b900
RanaReebaal <113040207+RanaReebaal@users.noreply.github.com>
2023-05-18 20:37:08 +0500

    Smart Contracts Basic

---
+++ Smart Contract Basic Local/contracts/test.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.18;
+
+contract Test {
+    function add(uint256 x, uint256 y) public pure returns (uint256){
+        return x + y;
+    }
+    
+    function subtract(uint256 x, uint256 y) public pure returns (uint256){
+        return x - y;
+    }
+}
